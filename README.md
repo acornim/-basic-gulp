107e3d114a4b227d7587ef1f5850a147e7354f58
acornim <youn_306@naver.com>
2020-04-01 13:37:05 +0900

    pug

--- README.md
+++ README.md
@@ -31,3 +31,28 @@ babel package 최신버전은 @붙음 - .babelrc preset 과정
 ```
 yarn add @babel/{register,core,preset-env}
 ```
+
+
+gulp는 task와 함께 작동함 -> task란? 모든 pug파일을 가지고 이것들을 다른 폴더에 집어넣으면.. 우선 얘들을 html으로 바꿔야겠지
+
+function을 export하거나 const하면 된당.
+scss파일을 css로 바꾸는 것. 
+이미지 최적화, js 압축, 모든 파일을 한 폴더에 넣고.. 여러 task 함꼐 작동가능
+
+pug 파일을 html으로 바꾸는 task 해보쟈 -> gulp pug plugin
+```
+yarn add gulp-pug -D //terminal
+import gulpPug from "gulp-pug"; //gulpfile
+```
+
+src()
+
+pipe()
+gulp는 pipe()랑 같이 사용. src()를 통해서 파일의 흐름을 만들고 pipe()로 그 흐름이 어디로 나오게 할지 만드는 것. 어떤 pipe는 코드를 컴파일하고, 다른 것은 코들르 복사, 코드를 최소화...등등
+
+dest() : destination으로 컴파일한 파일을 어느곳에 저장할지 정해주는 것
+
+
+build 폴더 내부 파일제거 
+컴파일했는데 수정하면 다시 리빌드해야짓
+먼저 터미널에 yarn add del -D로 설치하고, gulpfile.babel.js파일에 import
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,28 @@
 //import gulp from 'gulp'; gulpfile.js에서는 이거 안먹음.. 근데 이게 최신기술
 // gulpfile.js rename to gulpfile.babel.js
-import gulp from 'gulp';
-//const gulp = require("gulp");
+//const gulp = require("gulp"); 잘가~~
+import gulp from "gulp";
+import gpug from "gulp-pug";
+// 빌드한거 청소하고 다시 청소하자 
+import del from "del";
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+const pug = () =>
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]); //[]안에는 폴더이름
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+       
+//json 에서만 쓸 거 export 적어주면 됨
+export const dev = gulp.series([prepare, assets]); 
